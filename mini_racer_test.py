441767f08b40ac719a8184dd71808524aa4cf75e
Dan Chrostowski <dan@danchrostowski.com>
2019-11-21 09:24:52 -0600

    redefine atob fn

--- mini_racer_test.py
+++ mini_racer_test.py
@@ -8,6 +8,10 @@ var a = '.581.86'.split('').reverse().join('')
 var yxy = '\x4e\x54\x63\x75\x4e\x6a\x59\x3d'.replace(/\\x([0-9A-Fa-f]{2})/g, function () { return String.fromCharCode(parseInt(arguments[1], 16)) })
 var pp = (65 - ([] + []))/**//**//**/ +  15 - [] + []
 """
+
+def_atob = "var atob = function(arg) { return arg };"
+ratob = "var yxy = /* *//* *//* *//* */ atob('\x4d\x7a\x6b\x75\x4d\x54\x67\x3d'.replace(/\\x([0-9A-Fa-f]{2})/g,function(){return String.fromCharCode(parseInt(arguments[1], 16))}));"
+
 ctx.eval(js_code)
 addr1 = ctx.eval("a")
 port = ctx.eval("pp")
