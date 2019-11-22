6721098de75fea9faa16ca6672505372ba59ce5d
Dan Chrostowski <dan@danchrostowski.com>
2019-11-22 02:07:39 -0600

    completed deobfuscate method for proxydb.net

--- autoproxy/autoproxy/spiders/proxydb.py
+++ autoproxy/autoproxy/spiders/proxydb.py
@@ -4,9 +4,11 @@ import sys
 import logging
 from IPython import embed
 import re
-import json
+from py_mini_racer import py_mini_racer
 from proxy_objects import Proxy
 from storage_manager import StorageManager
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 class ProxydbSpider(scrapy.Spider):
     name = 'proxydb'
@@ -23,22 +25,46 @@ class ProxydbSpider(scrapy.Spider):
             logging.info("GET %s" % request.url)
             yield request
 
-    def parse(self,response):
-        with open('./proxydb.html','w') as ofh:
-            ofh.write(response.body.decode('utf-8'))
-        
-"""
-    def parse(self, response):
-        tds = response.xpath('//div[@class="table-responsive"]/table[contains(@class,"table-hover")]//tr/td')
-        embed()
-        for tr in trs:
-            address_port = tr.xpath('./td[1]/a/text()').extract_first()
-            embed()
-            protocol = tr.xpath('./td[5]/text()').extract_first()
-            ap_res = re.search(r'^([\d\.]+)\:(\d+)$', address_port)
-            address = ap_res.group(1)
-            port = int(ap_res.group(2))
-            embed()
+    def deobfuscate(self,resp):
 
-            self.storage_mgr.new_proxy(Proxy(address=address, port=port, protocol=protocol))
-"""
+    proxies = []
+
+    try:
+        ctx.eval("const atob = (arg) => arg")
+        rnnumt = resp.xpath('//div[@data-rnnumt]/@data-rnnumt').extract_first()
+        ctx.eval("let rnnumt = %s" % rnnumt)
+        
+        trs = resp.xpath('//div[@class="table-responsive"]/table[contains(@class,"table-hover")]/tbody/tr')
+        for tr in trs:
+            script = tr.xpath('td[1]/script/text()').extract_first()
+            string_to_replace = "(+document.querySelector('[data-rnnumt]').getAttribute('data-rnnumt'))"
+            script = script.replace(string_to_replace, " rnnumt ")
+            
+            scripts = script.split(';')[0:3]
+            var_re = r'var\s+(\w+)\s*\='
+
+            addr1_var = re.search(var_re, scripts[0]).group(1)
+            addr2_var = re.search(var_re, scripts[1]).group(1)
+            port_var = re.search(var_re, scripts[2]).group(1)
+
+            for js in scripts:
+                ctx.eval(js)
+
+            addr1 = ctx.eval(addr1_var)
+            addr2 = base64.b64decode(ctx.eval(addr2_var)).decode('utf-8')
+            port = int(ctx.eval(port_var))
+
+            address = "%s%s" % (addr1,addr2)
+            protocol = tr.xpath('td[5]/text()').extract_first().strip().lower()
+            logging.info("successfully deobfuscated proxy:\naddress=%s port=%s protocol=%s" % (address,port, protocol))
+            proxies.append({ 'address': address, 'port':port, 'protocol': protocol })
+
+    except Exception as e:
+        logging.warn(e)
+
+
+    return proxies
+
+    def parse(self,response):
+        proxies = deobfuscate(response)
+        
--- tmp/proxydb_test.py
+++ tmp/proxydb_test.py
@@ -1,8 +1,14 @@
+import sys
+import logging
 import scrapy
 from py_mini_racer import py_mini_racer
 from IPython import embed
 import base64
+import re
+
 ctx = py_mini_racer.MiniRacer()
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
 
 
 def get_mock_response():
@@ -12,14 +18,57 @@ def get_mock_response():
         url=req.url,
         request=req,
         body = file_content.encode('utf-8')
+    
     )
+
+def replace_str_dict():
+    return {
+        "(+document.querySelector('[data-rnnumt]').getAttribute('data-rnnumt'))": " rnnumtt ",
+
+    }
+
+
+def eval_block(block):
+    ctx.eval(block)
     
 def deobfuscate(resp):
-    script_tds = resp.xpath('//div[@class="table-responsive"]/table[contains(@class,"table-hover")]//tr/td[1]')
-    for td in script_tds:
-        script = td.xpath('script/text()').extract_first()
-        print("SCRIPT: %s " % script)
-        embed()
+
+
+    proxies = []
+
+    try:
+        ctx.eval("const atob = (arg) => arg")
+        rnnumt = resp.xpath('//div[@data-rnnumt]/@data-rnnumt').extract_first()
+        ctx.eval("let rnnumt = %s" % rnnumt)
+        
+        trs = resp.xpath('//div[@class="table-responsive"]/table[contains(@class,"table-hover")]/tbody/tr')
+        for tr in trs:
+            script = tr.xpath('td[1]/script/text()').extract_first()
+            string_to_replace = "(+document.querySelector('[data-rnnumt]').getAttribute('data-rnnumt'))"
+            script = script.replace(string_to_replace, " rnnumt ")
+            
+            scripts = script.split(';')[0:3]
+            var_re = r'var\s+(\w+)\s*\='
+
+            addr1_var = re.search(var_re, scripts[0]).group(1)
+            addr2_var = re.search(var_re, scripts[1]).group(1)
+            port_var = re.search(var_re, scripts[2]).group(1)
+
+            for js in scripts:
+                ctx.eval(js)
+
+            addr1 = ctx.eval(addr1_var)
+            addr2 = base64.b64decode(ctx.eval(addr2_var)).decode('utf-8')
+            port = int(ctx.eval(port_var))
+
+            address = "%s%s" % (addr1,addr2)
+            protocol = tr.xpath('td[5]/text()').extract_first().strip().lower()
+            logging.info("successfully deobfuscated proxy:\naddress=%s port=%s protocol=%s" % (address,port, protocol))
+            proxies.append({ 'address': address, 'port':port, 'protocol': protocol })
+
+    except Exception as e:
+        logging.warn(e)
+
 
 
 
