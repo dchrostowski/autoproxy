53914c82f39bd6e48e8331e80c345106bb4f0a7d
Dan Chrostowski <dan@danchrostowski.com>
2019-12-22 14:15:38 -0600

    finished proxylisty spider

--- autoproxy/autoproxy/spiders/proxylisty.py
+++ autoproxy/autoproxy/spiders/proxylisty.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from IPython import embed
+from storage_manager import StorageManager
+from proxy_objects import Proxy
 
 class ProxylistySpider(scrapy.Spider):
     name = 'proxylisty'
     allowed_domains = ['proxylisty.com']
     start_urls = ['http://www.proxylisty.com/ip-proxylist']
+
+    def __init__(self,*args,**kwargs):
+        self.count = int(kwargs.get('count',1))
+        self.storage_mgr = StorageManager()
 
     def parse(self, response):
 
@@ -13,9 +19,21 @@ class ProxylistySpider(scrapy.Spider):
         for tr in trs:
             tds = tr.xpath('td')
             address = tr.xpath('td[1]/text()').extract_first()
-            port = tr.xpath('td[2]text()').extract_first()
-            ptype = tr.xpath('td[3]/text()').extract_first()
-            print(address)
-            print(port)
-            print(ptype)
-            embed()
+            port = tr.xpath('td[2]/a/text()').extract_first()
+            protocol = tr.xpath('td[3]/text()').extract_first()
+
+            if address is not None and port is not None and protocol is not None:
+                try:
+                    proxy = Proxy(address=address,port=int(port),protocol=protocol.lower())
+                    print(proxy)
+                    self.storage_mgr.new_proxy(proxy)
+                except:
+                    pass
+
+
+        
+        next_link = response.xpath('//div[@id="content"]//table[1]/tr/td[@colspan="9"]/ul/li/a[text()="Next"]/@href').extract_first()
+        if next_link is not None:
+            yield scrapy.Request(url=response.urljoin(next_link),callback=self.parse)
+            
+            
