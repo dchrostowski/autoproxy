f0b5df5bfdf4187c49b0983f5c736a962e6501f4
Dan Chrostowski <dan@danchrostowski.com>
2020-02-04 02:03:25 -0600

    return None on RedisDetailQueueEmpty excpetion instead of request

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 docker-compose build scrapyd
-docker-compose build spider_scheduler
 docker-compose up -d scrapyd
+docker-compose build spider_scheduler
 docker-compose up -d spider_scheduler
 docker-compose logs -f
